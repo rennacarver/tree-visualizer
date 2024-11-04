e73928fd9cad1dbd96ba359e30aa939384bdb8ad
Renna Carver <87547832+rennacarver@users.noreply.github.com>
2024-11-03 21:15:44 -0800

    remove boilerplate css

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+import Image from 'next/image'
+import styles from './page.module.css'
+import Barchart from './components/Barchart/Barchart'
+
+export default function Home() {
+  return <Barchart></Barchart>
+}
